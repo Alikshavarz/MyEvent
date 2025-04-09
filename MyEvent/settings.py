bd9066d7e295f2c83b9116671586965bdaae7476
ali <realalikeshavarz@gmail.com>
2025-04-09 19:59:20 +0330

    liara added to project

--- MyEvent/settings.py
+++ MyEvent/settings.py
@@ -9,10 +9,13 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
+import dj_database_url
+from dotenv import load_dotenv
 from pathlib import Path
 from datetime import timedelta
-from .secretkey import *
+
+load_dotenv()
 
 
 
@@ -24,12 +27,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = secretkey
+SECRET_KEY = 'eed6606502e58fe12d5aee10ee94bd4313220058d61b5a91fde4d02e0e0e4af0c3ab9772'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['myevent.liara.run']
 
 
 # Application definition
@@ -99,11 +102,9 @@ WSGI_APPLICATION = 'MyEvent.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
+
 
 
 # Password validation
